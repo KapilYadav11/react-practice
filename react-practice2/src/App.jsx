8ed8249ee2d3e5d735c74226176e73ce93f72a2e
KapilYadav11 <kapily1011@gmail.com>
2026-01-29 19:53:38 +0530

    react-practice

--- react-practice2/src/App.jsx
+++ react-practice2/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import PostComponent from './PostComponent'
 import ToggleMessage from './ToggleMessage'
+import Post from './Post'
 
 const App = () => {
+
+  function addPst() {
+
+  }
   return (
     <div>
       {/* <ToggleMessage/> */}
-      <div style={{backgroundColor:'#bdc3c7', height: '100vh'}}>
+      <div style={{ backgroundColor: '#bdc3c7', height: '100vh' }}>
         {/* <h1>Undestanding about the components </h1> */}
         {/* <div style={{display: 'flex', justifyContent: 'center'}}>
           <div>
@@ -19,10 +24,23 @@ const App = () => {
           
           
         </div> */}
-        
-        
+        <button onClick={addPst}>Add Post</button>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div>
+            <Post />
+            name={"Kapil"}
+            subtitle={"10 followers"}
+            time={"2m ago"}
+            image={"https://tse3.mm.bing.net/th/id/OIP.983RG0ClupsMjDufJ6oHlwHaHa?pid=Api&P=0&h=180"
+            }
+            description={"What to know how to win big? Check out these folks won $5000 in bounties"}
+fjf
+          </div>
+        </div>
+
+
       </div>
-    </div>  
+    </div>
   )
 }
 
--- react-practice2/src/Post.jsx
+++ react-practice2/src/Post.jsx
@@ -4,21 +4,35 @@ const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderCo
 const Post = ({name, subtitle, time, image, description}) => {
     
   return (
-    <div style={{style}}>
-        <img src={image} style={{
-            width: 30,
-            height: 30,
-            borderRadius: 20
-        }} />
-        <div>
-            <b>
-                {name}
-            </b>
-            <div>{subtitle}</div>
-            {(time !== undefined) ? <div></div>:null}
-        </div>
-    </div>
+    <div style={{ style }}>
+  <img
+    src={image}
+    style={{
+      width: 30,
+      height: 30,
+      borderRadius: 20,
+    }}
+  />
+  <div>
+    <b>{name}</b>
+    <div>{subtitle}</div>
+    {time !== undefined ? (
+      <div style={{ display: "flex" }}>
+        <div>{time}</div>
+        <img
+          src={
+            "https://tse3.mm.bing.net/th/id/OIP.983RG0ClupsMjDufJ6oHlwHaHa?pid=Api&P=0&h=180"
+          }
+          style={{ width: 12, height: 12 }}
+        />
+      </div>
+    ) : null}
+  </div>
+  <div style={{ fontSize: 12}}>{description}</div>
+</div>
+
+    
   )
-}
+} 
 
 export default Post
