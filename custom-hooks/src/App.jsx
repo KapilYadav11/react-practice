b12e37629e2a2f1bbb690b2f881012ec25830eb4
KapilYadav11 <kapilyadav951641@gmail.com>
2026-02-15 23:59:39 +0530

    useDebounce

--- custom-hooks/src/App.jsx
+++ custom-hooks/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Counter } from './CustomHook'
 import {  useFetch, usePostTitle } from './UseFetch'
 import { usePrev } from './UsePrev'
+import { useDebounce } from './UseDebounce'
+
 
 const App = () => {
 
-  const[state , setState] = useState(0);
-  const prev = usePrev(state);
+  const[inputVal, setInputVal] = useState("");
+  const debouncedValue = useDebounce(inputVal, 200)
+
+  function change(e){
+    setInputVal(e.target.value)
+  }
+
+  useEffect(() =>{
+   
+  console.log("debounce opeartions")
+  }, [debouncedValue])
+
+  //code 2
+  // const[state , setState] = useState(0);
+  // const prev = usePrev(state);
   
+
+
+  //  code 1
   // const[currentPost , setCurrentPost] = useState(1);
   // const {finalData , loading } = useFetch("https://jsonplaceholder.typicode.com/todos/2" + currentPost)
 
@@ -22,18 +40,22 @@ const App = () => {
 
 return (
   <div>
-    <p>{state}</p>
+
+    <input type="text" onChange={change}></input>
+
+    {/* code 2 */}
+    {/* <p>{state}</p>
     <button onClick={() =>{
       setState(c => c+1)
     }}>
       Click me
     </button>
 
-    <p>The previous value was {prev}</p>
+    <p>The previous value was {prev}</p> */}
 
 
 
-
+    {/* code 1 */}
     {/* <Counter />
     <Counter />
     <Counter />
